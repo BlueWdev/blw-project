381e1e1b7f708f63c765a175f7bba243358370c9
bluewdev <strongcrood@gmail.com>
2023-06-04 20:34:35 +0300

    fixed route

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,7 +42,6 @@
                     <a class="navbar-brand" href="/">
                         <img src="{{asset('assets/logo/logo-mobile-landing-only.png')}}" alt="" width="80px">
                     </a>
-
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
